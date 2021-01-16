a29426f678d704553a5c901cee2b22c9449404d9
wan8234 <wan8234@naver.com>
2021-01-16 22:00:09 +0900

    make navbar sum

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,12 +2,19 @@ import React, { Component } from "react";
 
 class Navbar extends Component {
     render() {
+        const habits = this.props.habits;
+        var sum = 0;
+        habits.forEach((item) => {
+            sum += item.count;
+        });
+
         return (
             <div className="navbar">
                 <button className="logo-leaf">
-                    <i class="fas fa-leaf"></i>
+                    <i className="fas fa-leaf"></i>
                 </button>
                 <span className="habit-tracker">Habit Tracker</span>
+                <span className="habit-count habit-navbar-count">{sum}</span>
             </div>
         );
     }
